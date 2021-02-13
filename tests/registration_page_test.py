f7680f66b317c6f7d9e525785684adcf4f4b1d55
Karol Kolanski <karol.kolanski@wsb.wroclaw.pl>
2021-02-13 14:17:59 +0100

    Initial Registration test

---
+++ tests/registration_page_test.py
@@ -0,0 +1,12 @@
+import unittest
+from tests.base_test import BaseTest
+
+class RegistrationTest(BaseTest):
+    """
+    Test strony Rejestracji
+    """
+    def test_incorrect_email(self):
+        pass
+
+if __name__=="__main__":
+    unittest.main(verbosity=2)
