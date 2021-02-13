1abf4e3d161c8d37b90c0e21815c47fa9357853e
Karol Kolanski <karol.kolanski@wsb.wroclaw.pl>
2021-02-13 13:05:23 +0100

    Wait for ZALOGUJ_BTN added

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,11 +1,20 @@
 from pages.base_page import BasePage
 from locators import HomePageLocators
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class HomePage(BasePage):
     """
     Strona domowa
     """
-
     def click_zaloguj_btn(self):
-        element = self.driver.find_element(*HomePageLocators.ZALOGUJ_BTN)
+        # Tworzenie instancji klasy WebDriverWait
+        wait = WebDriverWait(self.driver, 60)
+        # Wywołanie metody until na obiekcie WebDriverWait
+        element = wait.until(EC.element_to_be_clickable(*HomePageLocators.ZALOGUJ_BTN))
+        # element = self.driver.find_element(*HomePageLocators.ZALOGUJ_BTN)
         element.click()
+
+    def _verify_page(self):
+        assert "Oficjalna strona Wizz Air" in self.driver.title
+
