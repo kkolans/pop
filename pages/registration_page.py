3b561391941f4c31c8473adee77b067f02e04463
Karol Kolanski <karol.kolanski@wsb.wroclaw.pl>
2021-02-13 13:42:42 +0100

    Base test added

---
+++ pages/registration_page.py
@@ -0,0 +1,16 @@
+from base_page import BasePage
+
+class RegistrationPage(BasePage):
+    """
+    Strona rejestracji
+    """
+    # Wpisanie imienia
+    def fill_name(self, name):
+        pass
+    # Wpisanie nazwiska
+    # Wybór płci
+    # Podanie kodu kraju
+    # Wpisanie nru telefonu
+    # Wpisanie e-maila
+    # Wpisanie hasła
+    # Wybór narodowości
